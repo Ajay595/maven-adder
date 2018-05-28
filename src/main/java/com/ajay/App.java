7b1c98721ca81c1ce04b86bf7d27dd0c7e1bb406
Ajay.A <39511172+Ajay595@users.noreply.github.com>
2018-05-28 12:10:35 +0000

    Update App.java

--- src/main/java/com/ajay/App.java
+++ src/main/java/com/ajay/App.java
@@ -9,9 +9,8 @@ public class App
     public static void main( String[] args )
     {
 	int x,y,s;
-	System.out.println("Numbers are ="+args[0]+" & "+args[1]);
-	x=Integer.parseInt(args[0]);
-	y=Integer.parseInt(args[1]);
+	x=10;
+	y=6;
 	s=x+y;
         System.out.println( "Sum = "+s);
     }
