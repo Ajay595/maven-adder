633e10d621740fc6e10d6234582c306aa16b7eae
Ajay.A <39511172+Ajay595@users.noreply.github.com>
2018-05-29 07:36:23 +0000

    Update App.java

--- src/main/java/com/ajay/App.java
+++ src/main/java/com/ajay/App.java
@@ -9,8 +9,8 @@ public class App
     public static void main( String[] args )
     {
 	int x,y,s;
-	x=10;
-	y=6;
+	x=Integer.parseInt(args[0]);
+	y=Integer.parseInt(args[1]);
 	s=x+y;
         System.out.println( "Sum = "+s);
     }
